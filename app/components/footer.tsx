255a8a272df06866e92b3fd09cbd92298b77735b
fitrisetia <fitrisetia2003@gmail.com>
2024-11-02 15:12:27 +0700

    Update and rename favColor.tsx to footer.tsx

---
+++ app/components/footer.tsx
@@ -0,0 +1,10 @@
+// app/components/Footer.tsx
+const Footer = () => {
+    return (
+        <footer className="bg-gray-800 text-white text-center p-4">
+            <p>&copy; {new Date().getFullYear()} Fitri Setiawati. All rights reserved.</p>
+        </footer>
+    );
+};
+
+export default Footer;
